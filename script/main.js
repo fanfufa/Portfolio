2a168adb39e7a8a05fe527693b8970205c4b97c0
fanfufa <joaopaulostradioto@gmail.com>
2024-05-07 10:33:08 -0300

    Estilizando os componentes e links ancoras criados

--- script/main.js
+++ script/main.js
@@ -33,6 +33,10 @@ function criarExperiencia(ano, cargo, lugar, descricao, competencias){
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-    criarExperiencia('2022 - 2023', 'Tecnico em Desenvolivmento Sistemas', 'COTIL', 'OI' , ['HTML', 'CSS', 'JavaScript'])
-    criarExperiencia('2022 - 2023', 'Tecnico em Desenvolivmento Sistemas', 'COTIL', 'OI' , ['HTML', 'CSS', 'JavaScript'])
+    criarExperiencia('2022 - 2023', 'Formação como Tecnico em Desenvolivmento Sistemas', 'Colégio Técnico de Limeira - COTIL', 'DESCRIÇAO' , ['HTML', 'CSS', 'JavaScript', 'Bootstrap', 'C#', 'Java', 'MySQL', 'PowerBI'])
+    criarExperiencia('2021 - atualmente', 'Formação em Hardware de Computadores', 'Academia do Hardware', 'DESCRIÇAO' , ['Manutenção hardwares', 'Conhecimento dos componentes de hardware do computador', 'Limpeza de computadores', 'Rede Computadores'])
+    criarExperiencia('2022 - atualmente', 'Linguagem Python', 'Curso em Vídeo e BroCode', 'DESCRIÇAO' , ['Python', 'PyAutoGUI'])
+    criarExperiencia('2023', 'Tecnico em manutenção de computadores', 'Laboratório dos Notebooks', 'DESCRIÇAO' , ['Manuntenção de notebooks', 'Teste de performace nos computadores', 'Formatações', 'Limpezas', 'Trocas de telas, memoria RAM e SSD/HD', 'Montagem e desmontagem de notebooks'])
+    criarExperiencia('2023 - atualmente', 'Help Desk', 'Alfa Networks', 'DESCRIÇAO' , ['Atendimento ao cliente', 'Suporte de impressoras e dispostivos SAT', 'Configuração de etiquetas PPLA e Zebra', 'Reunião e treinamento com clientes'])
+    criarExperiencia('2024', 'Desenvolvedor Front-End', 'Community Dreams Foundation', 'DESCRIÇAO' , ['React', 'HTML', 'CSS', 'JavaScript', 'Metodologia Sprint'])
 });
